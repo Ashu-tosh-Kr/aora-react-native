6380b87f83fd6779e34ece15f82ee7544709ae73
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2024-06-05 15:06:15 +0530

    setup complete

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-red-200">
-      <Text className="text-3xl">baby!</Text>
+      <Text className="text-3xl font-pblack">baby!</Text>
       <Link href="/profile">About</Link>
       <StatusBar style="auto" />
     </View>
