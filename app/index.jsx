5ee579d520539264191c2d6ca407daff74035beb
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2024-06-05 14:53:59 +0530

    tailwind setup

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-red-200">
+      <Text className="text-3xl">baby!</Text>
+      <Link href="/profile">About</Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
