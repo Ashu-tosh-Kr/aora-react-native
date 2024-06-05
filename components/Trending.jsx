bafea7b6953295b34b5d6f164c554510feab40e0
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2024-06-06 03:37:16 +0530

    setup horizontal flat list

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,14 +1,83 @@
-import { View, Text, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
 import React from "react";
+import { useState } from "react";
+import { icons } from "../constants";
+
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1.1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={300}
+    >
+      {play ? (
+        <Text className="text-white">{item.id}</Text>
+      ) : (
+        <TouchableOpacity
+          onPress={() => setPlay(true)}
+          className="relative justify-center items-center"
+          activeOpacity={0.7}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] overflow-hidden shadow-black/40 shadow-lg"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+};
 
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[1].$id);
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
   return (
     <FlatList
       data={posts}
-      keyExtractor={(itm) => itm.id}
+      keyExtractor={(itm) => itm.$id}
       renderItem={({ item }) => (
-        <Text className="ext-3xl text-white">{JSON.stringify(item.id)}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{ itemVisiblePercentThreshold: 70 }}
+      contentOffset={{ x: 170 }}
       horizontal
     />
   );
