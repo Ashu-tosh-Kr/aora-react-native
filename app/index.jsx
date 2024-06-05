fefac3d3b6441b4f9e7a9b6b1d04f857c68c20bc
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2024-06-05 16:08:23 +0530

    basic onboarding screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-red-200">
-      <Text className="text-3xl font-pblack">baby!</Text>
-      <Link href="/home">Home</Link>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="h-full bg-primary">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full h-full px-4 justify-center items-center">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl font-bold text-center text-white">
+              Discover endless possibilities with{" "}
+              <Text className="text-secondary-200">Melee!</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-gray-100 mt-7 text-center font-pregular text-sm">
+            Where creativity meet innovation: embark on a journey of limitless
+            exploration with Melee.
+          </Text>
+        </View>
+      </ScrollView>
+      <StatusBar style="light" />
+    </SafeAreaView>
   );
 }
