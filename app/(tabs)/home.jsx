3da565ed0299b30e536a0c7dafefdeb6fe023e69
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2024-06-05 20:57:32 +0530

    basic home setup

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,43 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList } from "react-native";
+import { images } from "../../constants";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ $id: 1 }]}
+        keyExtractor={(itm) => itm.$id}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">
+            {JSON.stringify(item.$id)}
+          </Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="items-start flex-row mb-6 justify-between">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="font-psemibold text-2xl text-white">
+                  Ashutosh
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                  source={images.logoSmall}
+                />
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
